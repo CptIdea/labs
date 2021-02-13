c472b2ade93fe525c754cbe35d43c8404ae10ea9
CptIdea <Captain5849>
2021-02-13 23:21:03 +0300

    lab4.2 fix

--- lab4/lab4.2.py
+++ lab4/lab4.2.py
@@ -10,6 +10,7 @@ ans = []
 for i in range(len(arr)):
     if i % 2 == 0:
         ans += arr[i]
+ans += ['raz',2]
 print(ans)
 
 print("======Вариант 3======")
@@ -17,7 +18,13 @@ if len(arr)<9:
     ans = "Список короче восьми элементов :("
 else:
     ans = arr[3:8]
-    for i in range(len(arr)):
-        if i % 2 == 0:
-            ans += arr[i]
+    ans += ['raz',2]
+print(ans)
+
+print("======Вариант 4======")
+ans = []
+ans += ['raz',2,3,4,5]
+for i in range(len(arr)):
+    if i % 2 == 0:
+        ans += arr[i]
 print(ans)

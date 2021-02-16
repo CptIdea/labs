bd3aff41ec607f3ed63d6feba72372cff7aabe03
CptIdea <pliyzik@yandex.ru>
2021-02-17 01:16:46 +0300

    fix 4.1

--- lab4/lab4.1.py
+++ lab4/lab4.1.py
@@ -1,9 +1,11 @@
 matrix = list(
     map(
-        lambda x: list(map(int,x.split())),
+        lambda x: list
+        (map(int,x.split())),
             input("Ввод матрицы.\nДелитель столбцов - ' '\nДелитель строк - ';'\n>").split(';')))
 
 sum = 0
+print(matrix)
 for s in matrix:
     for n in s:
         sum+=n
